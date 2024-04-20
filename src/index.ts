ce400d5088eaf171027c66c214d3537ff306a0e4
campos-maravilla <antonycampos25@outlook.com>
2024-04-20 13:46:14 -0600

    instalando-colors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import server from './server'
+import color from 'colors'
 
 const port=process.env.PORT || 4000
 
 server.listen(port,()=>{
-    console.log(`REST API funcionando en el puerto ${port}`)
-}
-)
+    console.log(color.cyan.bold(`REST API funcionando en el puerto ${port}`))
+} )
