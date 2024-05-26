20247933fbc215e46abc840f6203ef94cdc486cc
campos-maravilla <antonycampos25@outlook.com>
2024-05-26 17:43:00 -0600

    creando-el-router-para-autenticar

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import projectRoutes from './routes/projectRoutes'
 import cors from 'cors'
 import { corsConfig } from './config/cors'
 import morgan from 'morgan'
+import authRoutes from './routes/authRoutes'
 
 dotenv.config()
 
@@ -12,7 +13,7 @@ connectDB()
 
 const app=express()
 
-app.use(cors(corsConfig))
+//app.use(cors(corsConfig))
 
 // Logging 
 app.use(morgan('dev'))
@@ -20,5 +21,6 @@ app.use(morgan('dev'))
 app.use(express.json())//para poder leer datos del formulario
 
 app.use('/api/projects',projectRoutes)
+app.use('/api/auth',authRoutes)
 
 export default app 
