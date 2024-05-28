ac2c8cde3db2d2468aaf833a29ddac2f83662437
campos-maravilla <antonycampos25@outlook.com>
2024-05-28 07:46:54 -0600

    hashear-password,se crea la carpeta de utils en src y ahi se crea el archivo de auth.ts y se crea el metodo de hashPassword y se le pasa como parametro el password como string y se llama en AuthController.ts antes de guardar el usuario

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,19 @@
 
 import type { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
+
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         try {
+            const { password } = req.body
             const user = new User(req.body)
+
+            // Hash Password 
+            // const salt = await bcrypt.genSalt(10)
+            // user.password = await bcrypt.hash(password, salt)
+            user.password = await hashPassword(password)
             await user.save()
 
             res.send('Cuenta creada,revisa tu email para confirmarla')
