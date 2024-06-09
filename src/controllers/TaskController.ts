49f29b6f96905f10af2417e6560c7aa65e78a980
campos-maravilla <antonycampos25@outlook.com>
2024-06-09 07:24:10 -0600

    modificando-el-modelos-de-tarea-para-agregar-el-campo-completedby,se agrega el campo competedBy al modelo Task y en el controlador se hace una condicion en updateStatus,despues de req.task.status=status

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,89 +1,94 @@
 
-import type { Request,Response } from "express";
+import type { Request, Response } from "express";
 
 import Task from "../models/Task";
 
-export class TaskController{
-    static createTask=async(req:Request,res:Response)=>{
-        
+export class TaskController {
+    static createTask = async (req: Request, res: Response) => {
+
         try {
-            const task=new Task(req.body) 
+            const task = new Task(req.body)
             //task.proyect=>viene de task project.id es la instancia de project const project=await Project.findById(projectId))
             //req=>viene de middleware/project.ts
-            task.project=req.project.id
+            task.project = req.project.id
             req.project.tasks.push(task.id)//agregando la tarea al proyecto
-            
-         await   Promise.allSettled([task.save(),req.project.save()])
+
+            await Promise.allSettled([task.save(), req.project.save()])
             res.send('Tarea creada correctamente')
         } catch (error) {
-            res.status(500).json({error:'Hubo un error'})
+            res.status(500).json({ error: 'Hubo un error' })
         }
     }
-    
-    static getProjectTasks=async(req:Request,res:Response)=>{
+
+    static getProjectTasks = async (req: Request, res: Response) => {
         try {
             //populate('project')=>trae todo lo de project
-            const tasks=await Task.find({project:req.project.id}).populate('project')
+            const tasks = await Task.find({ project: req.project.id }).populate('project')
             res.json(tasks)
         } catch (error) {
-            res.status(500).json({error:'Hubo un error'})
+            res.status(500).json({ error: 'Hubo un error' })
         }
     }
-    
-    static getTaskById=async(req:Request,res:Response)=>{
-        try {
-       
-            res.json(req.task)
-        } catch (error) {
-             if (error.kind === 'ObjectId') {
-                const error = new Error('Tarea no Encontrada')
-                return res.status(404).json({ msg: error.message })
-            } 
-           // res.status(500).json({error:'Hubo un error'})
-        }
-    }
-    
-    static updateTask=async(req:Request,res:Response)=>{
-        try {
-            req.task.name=req.body.name
-            req.task.description=req.body.description
-            await req.task.save()
-            res.send('Tarea actualizada correctamente')
-            
-        } catch (error) {
-             /* if (error.kind === 'ObjectId') {
-                const error = new Error('Tarea no Encontrada')
-                return res.status(404).json({ msg: error.message })
-            }  */
-            res.status(500).json({error:'Hubo un error'})
-        }
-    }
-    
-    static deleteTask=async(req:Request,res:Response)=>{
-        try {
-            req.project.tasks= req.project.tasks.filter(task=>task.toString() !==req.task.id.toString())
-           
-            await Promise.allSettled([req.task.deleteOne(),req.project.save()])
-            res.send('Tarea Eliminada Correctamente')
-        } catch (error) {
-             /* if (error.kind === 'ObjectId') {
-                const error = new Error('Tarea no Encontrada')
-                return res.status(404).json({ msg: error.message })
-            }  */
-            res.status(500).json({error:'Hubo un error'})
-        }
-    }
-    
-    static updateStatus=async(req:Request,res:Response)=>{
-        
+
+    static getTaskById = async (req: Request, res: Response) => {
         try {
 
-            const {status}=req.body
-            req.task.status=status 
+            res.json(req.task)
+        } catch (error) {
+            if (error.kind === 'ObjectId') {
+                const error = new Error('Tarea no Encontrada')
+                return res.status(404).json({ msg: error.message })
+            }
+            // res.status(500).json({error:'Hubo un error'})
+        }
+    }
+
+    static updateTask = async (req: Request, res: Response) => {
+        try {
+            req.task.name = req.body.name
+            req.task.description = req.body.description
+            await req.task.save()
+            res.send('Tarea actualizada correctamente')
+
+        } catch (error) {
+            /* if (error.kind === 'ObjectId') {
+               const error = new Error('Tarea no Encontrada')
+               return res.status(404).json({ msg: error.message })
+           }  */
+            res.status(500).json({ error: 'Hubo un error' })
+        }
+    }
+
+    static deleteTask = async (req: Request, res: Response) => {
+        try {
+            req.project.tasks = req.project.tasks.filter(task => task.toString() !== req.task.id.toString())
+
+            await Promise.allSettled([req.task.deleteOne(), req.project.save()])
+            res.send('Tarea Eliminada Correctamente')
+        } catch (error) {
+            /* if (error.kind === 'ObjectId') {
+               const error = new Error('Tarea no Encontrada')
+               return res.status(404).json({ msg: error.message })
+           }  */
+            res.status(500).json({ error: 'Hubo un error' })
+        }
+    }
+
+    static updateStatus = async (req: Request, res: Response) => {
+        // req.task.completedBy = req.user.id para saber quien cambio el status de la tarea
+        try {
+
+            const { status } = req.body
+            req.task.status = status
+            if (status === 'pending') {
+                req.task.completedBy = null
+            } else {
+                req.task.completedBy = req.user.id
+            }
             await req.task.save()
             res.send('Tarea Actualizada')
         } catch (error) {
-            res.status(500).json({error:'Hubo un error'})
+            res.status(500).json({ error: 'Hubo un error' })
         }
     }
 }
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,44 +1,50 @@
 
-import mongoose,{Schema,Document, Types} from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 
-const taskStatus={
-    PENDING:'pending',
-    ON_HOLD:'onHold',
-    IN_PROGRESS:'inProgress',
-    UNDER_REVIEW:'underReview',
-    COMPLETED:'completed'
+const taskStatus = {
+    PENDING: 'pending',
+    ON_HOLD: 'onHold',
+    IN_PROGRESS: 'inProgress',
+    UNDER_REVIEW: 'underReview',
+    COMPLETED: 'completed'
 } as const
 
-export type TaskStatus=typeof taskStatus[keyof typeof taskStatus]
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
 
-export interface ITask extends Document  {
-    name:string
-    description:string
-    project:Types.ObjectId
-    status:TaskStatus
+export interface ITask extends Document {
+    name: string
+    description: string
+    project: Types.ObjectId
+    status: TaskStatus,
+    completedBy: Types.ObjectId
 }
 
-export const TaskSchema:Schema=new Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true
+export const TaskSchema: Schema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
     },
-    description:{
-        type:String,
-        trim:true,
-        required:true,
+    description: {
+        type: String,
+        trim: true,
+        required: true,
     },
-    project:{
-        type:Types.ObjectId,
-        ref:'Project'
+    project: {
+        type: Types.ObjectId,
+        ref: 'Project'
     },
-    status:{
-        type:String,
-        enum:Object.values(taskStatus),
-        default:taskStatus.PENDING
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING
+    },
+    completedBy: {
+        type: Types.ObjectId,
+        ref: 'User',
+        default: null
     }
-},{timestamps:true})
+}, { timestamps: true })
 
-const Task=mongoose.model<ITask>('Task',TaskSchema)
+const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task 
