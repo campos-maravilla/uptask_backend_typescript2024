b8dd0543fbb059c02e9d6384e4f34dd6ce1edf84
campos-maravilla <antonycampos25@outlook.com>
2024-06-10 16:52:08 -0600

    creando-endpoint-para-eliminar-notas,para eliminar se usa deleteOne y para que elimine tambien de las tareas se usa req.task.notes=req.task.notes.filter

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -139,4 +139,10 @@ router.post('/:projectId/tasks/:taskId/notes',
 router.get('/:projectId/tasks/:taskId/notes',
     NoteController.getTaskNotes
 )
+
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+    param('noteId').isMongoId().withMessage('ID no vàlido'),
+    handleInputErrors,
+    NoteController.deleteNote
+)
 export default router 
